0db0ec68a09a4e0bff1e3c1b7a87ec00b9cf57a8
Mary T Welch <mary.welch@artisantechgroup.com>
2021-04-03 20:24:55 -0500

    implementing DbContext class

---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,13 @@
+﻿using MarysOdeToFood.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
