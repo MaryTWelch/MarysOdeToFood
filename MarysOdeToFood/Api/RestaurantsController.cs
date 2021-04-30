a62f07076de63cba2d5e1761b290f22724b87fdb
Mary T Welch <mary.welch@artisantechgroup.com>
2021-04-29 23:52:41 -0500

    adding .Api to namespace

--- MarysOdeToFood/Api/RestaurantsController.cs
+++ MarysOdeToFood/Api/RestaurantsController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using MarysOdeToFood.Core;
 using OdeToFood.Data;
 
-namespace MarysOdeToFood
+namespace MarysOdeToFood.Api
 {
     [Route("api/[controller]")]
     [ApiController]
